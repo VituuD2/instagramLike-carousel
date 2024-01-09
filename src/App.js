6ca11f7566b5b3db92d389da011b1684914fa7a2
vituud2 <overroot0@gmail.com>
2024-01-09 11:28:22 -0300

    V1.3.3 09-01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style/App.css';
 import './awesome6/css/all.css'
+// import Navbar from './Navbar';
 import Carousel from './Carousel';
 
 // Importa todas as imagens da pasta
@@ -10,8 +11,9 @@ const imagesSrc = importAll(require.context('./assets', false, /\.(png)$/));
 const App = () => {
   return (
     <div className="App">
-      <h1> <i class="fab fa-instagram"></i> Carousel tester </h1>
+      <h1> <i className="fab fa-instagram"></i> Carousel tester </h1>
       <Carousel imagesSrc={imagesSrc} />
+      {/* <Navbar /> */}
     </div>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './style/Navbar.css';
 import './awesome6/css/all.css'
-import Carousel from './Carousel';
+// import Carousel from './Carousel';
 
-return(
+const Navbar = () => {
+    return(
+    <div className='Navbar'>
+        <nav>
+            <ul>
+                <li>CAROUSEL 1</li>
+                <li>CAROUSEL 2</li>
+                <li>CAROUSEL 3</li>
+                <li>CAROUSEL 4</li>
+                <li>CAROUSEL 5</li>
+                <li>CAROUSEL 6</li>
+                <li>CAROUSEL 7</li>
+                <li>CAROUSEL 8</li>
+                <li>CAROUSEL 9</li>
+                <li>CAROUSEL 10</li>
+            </ul>
+        </nav>
+    </div>
+    );
+};
 
-);
+export default Navbar;
