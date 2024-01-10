6606137466c4fe0dd192ebb2144cea3c53274999
vituud2 <overroot0@gmail.com>
2024-01-10 16:15:55 -0300

    Updates

---
+++ precache-manifest.6e28de9edb2df29e0e7af00e02dda147.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180cb6fca4e54f1096c123061ce7885e",
+    "url": "/instagramLike-carousel/index.html"
+  },
+  {
+    "revision": "321741ece7b318ea876f",
+    "url": "/instagramLike-carousel/static/css/main.cdf38692.chunk.css"
+  },
+  {
+    "revision": "85eb4f57df3e3ff86231",
+    "url": "/instagramLike-carousel/static/js/2.2c4e6fb1.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/instagramLike-carousel/static/js/2.2c4e6fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5222a7fb8bbe12c6cc64",
+    "url": "/instagramLike-carousel/static/js/3.b890cc72.chunk.js"
+  },
+  {
+    "revision": "321741ece7b318ea876f",
+    "url": "/instagramLike-carousel/static/js/main.3b830214.chunk.js"
+  },
+  {
+    "revision": "3950834a43e548b3a428",
+    "url": "/instagramLike-carousel/static/js/runtime-main.f0ebf2dc.js"
+  },
+  {
+    "revision": "09c09f36bfd6984954a31f8139d32f73",
+    "url": "/instagramLike-carousel/static/media/3_01.09c09f36.png"
+  },
+  {
+    "revision": "0c2d7739382f2e913d8445a29276961a",
+    "url": "/instagramLike-carousel/static/media/3_02.0c2d7739.png"
+  },
+  {
+    "revision": "5135438be8ef16d7da34e08f608eefef",
+    "url": "/instagramLike-carousel/static/media/3_03.5135438b.png"
+  },
+  {
+    "revision": "25026f156afd2062cd540b8027ef5c33",
+    "url": "/instagramLike-carousel/static/media/3_04.25026f15.png"
+  },
+  {
+    "revision": "25266b96f1e76bf3c6a587df6682398c",
+    "url": "/instagramLike-carousel/static/media/3_05.25266b96.png"
+  },
+  {
+    "revision": "e96ee675abd06bb6a69d5e7f50013266",
+    "url": "/instagramLike-carousel/static/media/3_06.e96ee675.png"
+  },
+  {
+    "revision": "b8c81026587846225c3ee8326cb10016",
+    "url": "/instagramLike-carousel/static/media/4_01.b8c81026.png"
+  },
+  {
+    "revision": "777e4f347181ab0b6a8d72d554700e3f",
+    "url": "/instagramLike-carousel/static/media/4_02.777e4f34.png"
+  },
+  {
+    "revision": "8e6f7d43cd1771787bb245f224a816aa",
+    "url": "/instagramLike-carousel/static/media/4_03.8e6f7d43.png"
+  },
+  {
+    "revision": "40fc889fc6f1170fc70977f797363035",
+    "url": "/instagramLike-carousel/static/media/4_04.40fc889f.png"
+  },
+  {
+    "revision": "3ab5dea3a36fd6b08ae66539e297c391",
+    "url": "/instagramLike-carousel/static/media/4_05.3ab5dea3.png"
+  },
+  {
+    "revision": "82c91ea430a12508c2532a4d975f251d",
+    "url": "/instagramLike-carousel/static/media/4_06.82c91ea4.png"
+  },
+  {
+    "revision": "92565cb436410690fe795762e05ca10e",
+    "url": "/instagramLike-carousel/static/media/TJztmXpWTmS.92565cb4.png"
+  },
+  {
+    "revision": "05c475fabceebae1f9d40ba6711cb41a",
+    "url": "/instagramLike-carousel/static/media/fa-brands-400.05c475fa.eot"
+  },
+  {
+    "revision": "5d875e6a4dcceea77fa7d4b8e1440982",
+    "url": "/instagramLike-carousel/static/media/fa-brands-400.5d875e6a.woff"
+  },
+  {
+    "revision": "7c0be8b6640f024b6f4505161bf1bfd4",
+    "url": "/instagramLike-carousel/static/media/fa-brands-400.7c0be8b6.woff2"
+  },
+  {
+    "revision": "8ca6e74eddaf8fce3cdcf8647b37cbf1",
+    "url": "/instagramLike-carousel/static/media/fa-brands-400.8ca6e74e.ttf"
+  },
+  {
+    "revision": "e2129c0510f0339c77891735feeacc1b",
+    "url": "/instagramLike-carousel/static/media/fa-brands-400.e2129c05.svg"
+  },
+  {
+    "revision": "3d2ab213cff7f2f1f2f26a25dee7a8d3",
+    "url": "/instagramLike-carousel/static/media/fa-duotone-900.3d2ab213.svg"
+  },
+  {
+    "revision": "6bd3ad9dbce9c232c66bfaee1e4c374f",
+    "url": "/instagramLike-carousel/static/media/fa-duotone-900.6bd3ad9d.eot"
+  },
+  {
+    "revision": "7bc112bc25c528dc4bbfec047c69d338",
+    "url": "/instagramLike-carousel/static/media/fa-duotone-900.7bc112bc.woff"
+  },
+  {
+    "revision": "c877e45db0614bba8e121510162149fc",
+    "url": "/instagramLike-carousel/static/media/fa-duotone-900.c877e45d.woff2"
+  },
+  {
+    "revision": "d80e843337fcdf622266a3d6d0168991",
+    "url": "/instagramLike-carousel/static/media/fa-duotone-900.d80e8433.ttf"
+  },
+  {
+    "revision": "0b4313e036624b29c8f651d0c9a5ecd8",
+    "url": "/instagramLike-carousel/static/media/fa-light-300.0b4313e0.woff"
+  },
+  {
+    "revision": "0e0b8b741276d5220c847d0be486609e",
+    "url": "/instagramLike-carousel/static/media/fa-light-300.0e0b8b74.ttf"
+  },
+  {
+    "revision": "3d8af27a47bbfab857f96dbcf710b087",
+    "url": "/instagramLike-carousel/static/media/fa-light-300.3d8af27a.woff2"
+  },
+  {
+    "revision": "a624ea3f4dd832cb54ea41286ded8fc3",
+    "url": "/instagramLike-carousel/static/media/fa-light-300.a624ea3f.eot"
+  },
+  {
+    "revision": "b51b241c5d3754e30868d61057beb228",
+    "url": "/instagramLike-carousel/static/media/fa-light-300.b51b241c.svg"
+  },
+  {
+    "revision": "2b8a79b1e1968ee628a76f96eb44e1b6",
+    "url": "/instagramLike-carousel/static/media/fa-regular-400.2b8a79b1.svg"
+  },
+  {
+    "revision": "3e332211899360ab9380df4d50e1c33a",
+    "url": "/instagramLike-carousel/static/media/fa-regular-400.3e332211.woff2"
+  },
+  {
+    "revision": "4776aa52b579295cf651f6284793dac4",
+    "url": "/instagramLike-carousel/static/media/fa-regular-400.4776aa52.woff"
+  },
+  {
+    "revision": "faa0b37d4fdba1e304982f192c55bbe2",
+    "url": "/instagramLike-carousel/static/media/fa-regular-400.faa0b37d.ttf"
+  },
+  {
+    "revision": "fde04f8e6967b818c6fb3338d8b366d3",
+    "url": "/instagramLike-carousel/static/media/fa-regular-400.fde04f8e.eot"
+  },
+  {
+    "revision": "150120b3d9b448dd116680b66eba9bbe",
+    "url": "/instagramLike-carousel/static/media/fa-solid-900.150120b3.woff2"
+  },
+  {
+    "revision": "1687b08928dc7cf6a3d1de0612470fcb",
+    "url": "/instagramLike-carousel/static/media/fa-solid-900.1687b089.svg"
+  },
+  {
+    "revision": "6c207a7b79c06c76e915eb8f30e51d8a",
+    "url": "/instagramLike-carousel/static/media/fa-solid-900.6c207a7b.eot"
+  },
+  {
+    "revision": "749dd3651c1a97f03faa88723af2371a",
+    "url": "/instagramLike-carousel/static/media/fa-solid-900.749dd365.ttf"
+  },
+  {
+    "revision": "fb2f453136d024f47f2463d0d2ea2f05",
+    "url": "/instagramLike-carousel/static/media/fa-solid-900.fb2f4531.woff"
+  }
+]);
